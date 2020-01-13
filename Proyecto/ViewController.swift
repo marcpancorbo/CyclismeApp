6b7ba8aa7695b23a088a612dd1a116da06849ee7
marcpancorbo <55668102+marcpancorbo@users.noreply.github.com>
2020-01-13 17:31:17 +0100

    Initial Commit

---
+++ Proyecto/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  UF2Pract1
+//
+//  Created by Professor on 26/11/2019.
+//  Copyright © 2019 DAM2T. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
