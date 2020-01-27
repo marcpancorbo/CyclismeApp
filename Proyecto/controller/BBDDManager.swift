211ec21e82b15b9301f477c10055c405a0860414
marcpancorbo <55668102+marcpancorbo@users.noreply.github.com>
2020-01-27 17:56:26 +0100

    Race model hecho, BBDDManager en proceso

---
+++ Proyecto/controller/BBDDManager.swift
@@ -0,0 +1,9 @@
+//
+//  BBDDManager.swift
+//  Proyecto
+//
+//  Created by Marc Pancorbo Montaño on 27/01/2020.
+//  Copyright © 2020 DAM2T. All rights reserved.
+//
+
+import Foundation
---
+++ Proyecto/model/Race.swift
@@ -0,0 +1,22 @@
+//
+//  Event.swift
+//  Proyecto
+//
+//  Created by Marc Pancorbo Montaño on 27/01/2020.
+//  Copyright © 2020 DAM2T. All rights reserved.
+//
+
+import Foundation
+
+class Event {
+    private var image : String;
+    private var name : String;
+    private var popularity : Int;
+    private var country : String;
+    private var roads : Int;
+    private var kilometers : Int;
+    
+    init(name : String, popularity : Int, ) {
+        <#statements#>
+    }
+}
