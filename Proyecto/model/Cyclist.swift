a1394e9367f1a110c0e6463a767b185ffa0e036b
arnau <arnausolans13@gmail.com>
2020-02-04 10:50:58 +0100

    Tabla del ciclista hecha y calse cicilista

--- Proyecto/model/Cyclist.swift
+++ Proyecto/model/Cyclist.swift
@@ -6,27 +6,30 @@
 ////  Copyright © 2020 DAM2T. All rights reserved.
 ////
 //
-//import Foundation
-//
-//class Cyclist{
-//
-//    private var id : Int
-//    private var team_id : Int
-//    private var country_id : Int
-//    private var specialty_id : Int
-//    private var lastname : String
-//    private var firstname : String
-//    private var birthdate : Date
-//    private var popularity : Int
-//    private var leader : Bool
-//    private var size : Decimal
-//    private var weight : Decimal
-//    private var mountain : Int
-//    private var plain : Int
-//    private var downhilling : Int
-//    private var sprint : Int
-//    private var resistante : Int
-//    private var recuperation : Int
-//    private var timetrial : Int
-//
-//}
+import Foundation
+import RealmSwift
+
+class Cyclist : Object{
+
+    @objc dynamic var id : Int = 0
+    @objc dynamic var team_id : Int = 0
+    @objc dynamic var country_id : Int = 0
+    @objc dynamic var specialty_id : Int = 0
+    @objc dynamic var lastname : String = ""
+    @objc dynamic var firstname : String = ""
+    @objc dynamic var birthdate : String = ""
+    @objc dynamic var popularity : Int = 0
+    @objc dynamic var leader : Bool = false
+    @objc dynamic var size : Int = 0
+    @objc dynamic var weight : Int = 0
+    @objc dynamic var mountain : Int = 0
+    @objc dynamic var plain : Int = 0
+    @objc dynamic var downhilling : Int = 0
+    @objc dynamic var sprint : Int = 0
+    @objc dynamic var resistante : Int = 0
+    @objc dynamic var recuperation : Int = 0
+    @objc dynamic var timetrial : Int = 0
+    
+    required init() {
+    }
+}
