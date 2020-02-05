397b808f7c6559dc03e2cfd666c56a6edd32bcfc
marcpancorbo <55668102+marcpancorbo@users.noreply.github.com>
2020-02-05 13:09:48 +0100

    Botones

--- Proyecto/controller/DetailCyclistViewController.swift
+++ Proyecto/controller/DetailCyclistViewController.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-
+import Floaty
 protocol CyclistDelegate {
     func delegateMethod(text : String)
 }
@@ -33,7 +33,8 @@ class DetailCyclistViewController: UIViewController, CyclistDelegate{
     @IBOutlet weak var recuperation: UILabel!
     @IBOutlet weak var timetrial: UILabel!
     @IBOutlet weak var surname: UILabel!
-    
+    private var buttonEdit = Floaty()
+    private var buttonSave = Floaty()
     private var manager = BBDDManager.getInstance()
     public var cyclistId: Int?
     private var cyclist: Cyclist? = nil
@@ -41,43 +42,60 @@ class DetailCyclistViewController: UIViewController, CyclistDelegate{
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        buttonEdit.buttonImage = UIImage(named: "icons8-edit")
+        buttonEdit.buttonColor = UIColor.orange
+        buttonEdit.addItem(icon: nil, handler: {item in
+            print("edit")
+        })
+        buttonEdit.handleFirstItemDirectly = true
+        buttonEdit.paddingX = 105
+        buttonEdit.paddingY = 90
+        buttonSave.buttonImage = UIImage(named: "icons8-checkmark")
+        buttonSave.buttonColor = UIColor.green
+        buttonSave.addItem(icon: nil, handler: {item in
+            print("save")
+        })
+        buttonSave.handleFirstItemDirectly = true
+        buttonSave.paddingY = 90
+        view.addSubview(buttonEdit)
+        view.addSubview(buttonSave)
         if (cyclistId == nil){
             imageCyclist.image = UIImage(named: "imageCyclist")
         }else{
             cyclist = manager.getCyclistById(id: cyclistId!)
-                   imageCyclist.image = UIImage(named: "imageCyclist")
-                   name.text = "Name: " + cyclist!.firstname
-                   surname.text = "Lastname: " + cyclist!.lastname
-                   country.text = "Country: " + String(cyclist!.country_id)
-                   speciality.text = "Speciality: " + String(cyclist!.specialty_id)
-                   team.text = "Team: " + String(cyclist!.team_id)
-                   birthday.text = "Birthday: " + cyclist!.birthdate
-                   size.text = "Size: " + String(cyclist!.size) + " cm"
-                   wight.text = "Weight: " + String(cyclist!.weight) + " kg"
-                   mountain.text = "Mountain: " + String(cyclist!.mountain)
-                   plain.text = "Plain: " + String(cyclist!.plain)
-                   downhilling.text = "Downhilling: " + String(cyclist!.downhilling)
-                   sprint.text = "Sprint: " + String(cyclist!.sprint)
-                   resistance.text = "Resistance: " + String(cyclist!.resistante)
-                   recuperation.text = "Recuperation: " + String(cyclist!.recuperation)
-                   timetrial.text = "Timetrial: " + String(cyclist!.timetrial)
+            imageCyclist.image = UIImage(named: "imageCyclist")
+            name.text = "Name: " + cyclist!.firstname
+            surname.text = "Lastname: " + cyclist!.lastname
+            country.text = "Country: " + String(cyclist!.country_id)
+            speciality.text = "Speciality: " + String(cyclist!.specialty_id)
+            team.text = "Team: " + String(cyclist!.team_id)
+            birthday.text = "Birthday: " + cyclist!.birthdate
+            size.text = "Size: " + String(cyclist!.size) + " cm"
+            wight.text = "Weight: " + String(cyclist!.weight) + " kg"
+            mountain.text = "Mountain: " + String(cyclist!.mountain)
+            plain.text = "Plain: " + String(cyclist!.plain)
+            downhilling.text = "Downhilling: " + String(cyclist!.downhilling)
+            sprint.text = "Sprint: " + String(cyclist!.sprint)
+            resistance.text = "Resistance: " + String(cyclist!.resistante)
+            recuperation.text = "Recuperation: " + String(cyclist!.recuperation)
+            timetrial.text = "Timetrial: " + String(cyclist!.timetrial)
         }
-       
         
         
-
+        
+        
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
